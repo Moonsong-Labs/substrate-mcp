362c5fc09154ebec49ee4661fe811a57dd6067d5
elfedy <federico@moonsonglabs.com>
2025-09-01 10:25:51 -0300

    Add panic wrapper for tools

--- src/service/utils.rs
+++ src/service/utils.rs
@@ -1,4 +1,7 @@
+use futures::FutureExt;
+use rmcp::model::CallToolResult;
 use rmcp::ErrorData as McpError;
+use std::panic::AssertUnwindSafe;
 
 pub fn mcp_error_internal(message: String) -> McpError {
     McpError {
@@ -14,4 +17,27 @@ pub fn mcp_error_invalid_params(message: String) -> McpError {
         message: message.into(),
         data: None,
     }
+}
+
+/// Executes a handler and returns mcp error on panic.
+pub async fn catch_panic_as_mcp_error<F>(future: F) -> Result<CallToolResult, McpError>
+where
+    F: std::future::Future<Output = Result<CallToolResult, McpError>> + Send,
+{
+    AssertUnwindSafe(future)
+        .catch_unwind()
+        .await
+        .unwrap_or_else(|panic| {
+            let panic_msg = if let Some(s) = panic.downcast_ref::<String>() {
+                s.clone()
+            } else if let Some(s) = panic.downcast_ref::<&str>() {
+                s.to_string()
+            } else {
+                "Unknown panic".to_string()
+            };
+
+            Err(mcp_error_internal(format!(
+                "Call panicked: {panic_msg}. This is a bug. Please report it to substrate-mcp mantainers.",
+            )))
+        })
 }
